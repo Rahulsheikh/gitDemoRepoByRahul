aeeabec8f2c8db18df1e7e36d6d666ae9f479532
Rahulsheikh <rahulsheikh94@gmail.com>
2020-06-25 14:04:40 +0530

    test 1

--- src/main/java/testjava.java
+++ src/main/java/testjava.java
@@ -2,5 +2,6 @@ public class testjava {
     public static void main(String[] args) {
         System.out.println("this is testjava class");
         System.out.println("bhds");
+        System.out.println(" ");
     }
 }
