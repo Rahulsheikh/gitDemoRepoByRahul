a2dd251fa0006cd89ae68a0ee99e0e1b6ce92d48
Rahulsheikh <rahulsheikh94@gmail.com>
2020-06-25 15:09:12 +0530

    test 2

--- src/main/java/testjava.java
+++ src/main/java/testjava.java
@@ -2,6 +2,11 @@ public class testjava {
     public static void main(String[] args) {
         System.out.println("this is testjava class");
         System.out.println("bhds");
-        System.out.println(" ");
+
+
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
     }
 }
