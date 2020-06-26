f7e102b88524e28bfc65f19d5cedf2f88f1c9249
Rahulsheikh <rahulsheikh94@gmail.com>
2020-06-26 17:14:08 +0530

    my new

--- src/main/java/testjava.java
+++ src/main/java/testjava.java
@@ -2,7 +2,10 @@ public class testjava {
     public static void main(String[] args) {
         System.out.println("this is testjava class");
         System.out.println("bhds");
+
+
         System.out.println("1");
         System.out.println();
+        System.out.println("new");
     }
 }
